d9fd135b13ea16ebb47098f66b44543fa8eb2990
nightfire36 <57152643+nightfire36@users.noreply.github.com>
2019-12-28 11:46:11 +0100

    v1.0

---
+++ src/saper/NewGameButton.java
@@ -0,0 +1,26 @@
+package saper;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class NewGameButton extends JButton implements ActionListener {
+	
+	JFrame frame;
+	
+	public NewGameButton(JFrame frame) {
+		super("New game");
+		this.frame = frame;
+		addActionListener(this);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent action)
+	{
+		frame.setVisible(false);
+		frame.dispose();
+		new Window().initialize();
+	}
+}
---
+++ src/saper/Saper.java
@@ -0,0 +1,10 @@
+package saper;
+
+public class Saper {
+	
+	public static void main(String[] args)
+	{
+		new Window().initialize();
+	}
+
+}
